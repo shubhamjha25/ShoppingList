5df650762960ccca3340898a4afe1e907c7a6b7c
Shubham Jha <shubham.jha2001m@gmail.com>
2021-01-06 02:10:56 +0530

    Redux Integration Part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import ShoppingList from './components/ShoppingList';
 
 import AppNav from './components/AppNav'
 
 function App() {
   return (
-    <div className="App">
-      <AppNav />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+        <div className="App">
+            <AppNav />
+            <ShoppingList />
+        </div>
+    </Provider>
   );
 }
 
---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+
+const initialState = {
+    items : [
+        { id: uuidv4(), name:'Eggs' },
+        { id: uuidv4(), name:'Milk' },
+        { id: uuidv4(), name:'Paneer' },
+        { id: uuidv4(), name:'Chicken' }
+    ]
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case GET_ITEMS: 
+            return {
+                ...state
+            }
+        default: return state;
+    }
+}
